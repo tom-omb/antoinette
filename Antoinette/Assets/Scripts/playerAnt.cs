67a5785a8ae793ef2c4b22bd3ca90fef96776406
Samoooooooooooooooooooooooooooooo <asmaibra.cs@gmail.com>
2025-03-15 18:33:40 +0300

    Living room, crouch and jump fixed
    1- living room scene
    2- crouch is finished: colliders AND speed, the interaction "press" with the button is enabled as well
    3- jumping animation is perfectly fixed

--- Antoinette/Assets/Scripts/playerAnt.cs
+++ Antoinette/Assets/Scripts/playerAnt.cs
@@ -7,18 +7,35 @@ public class playerAnt : MonoBehaviour
 {
     public Rigidbody2D rb;
     public Animator animator;
-    Vector2 moveInput;
+    public CircleCollider2D headPosition;
+    public CapsuleCollider2D bodySize;
+    private float currentSpeed;
+
 
     // Headers are to keep track of all of our public parameters
     [Header("Movement")]
-    public float moveSpeed = 5f;
+    public float moveSpeed = 1f;
     float horizontalMovement;
     bool isFacingRight = true; //our default
+    
 
     [Header("Jumping")]
     public float jumpPower = 3f;
-int jumps;
-int jumpNumber= 1;
+    int jumps;
+    int jumpNumber= 1;
+
+
+    [Header("Crouching")] 
+    public float crouchSpeed = 0.5f;
+    private bool isCrouching = false; //to calc current speed and enable animation
+
+    // offset= head;circle , size= body;capsule : 
+    public Vector2 standingColliderSize = new Vector2(3.71f, 2.27f);  //default capsule SIZE
+    public Vector2 crouchingColliderSize = new Vector2(3.71f, 0.98f); //new size/smaller capsule when crouching
+    
+    public Vector2 standingColliderOffset = new Vector2(0.71f, -0.24f); //default circle POSITION
+    public Vector2 crouchingColliderOffset = new Vector2(0.23f, -1.17f); //new offset/lower circle when crouching
+
 
     [Header("GroundCheck")]
     public Transform groundCheckPos;
@@ -26,37 +43,37 @@ int jumpNumber= 1;
     public LayerMask groundLayer;
 
 
-
+    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         animator =  GetComponent<Animator>();
+        headPosition = GetComponent<CircleCollider2D>();
+        bodySize = GetComponent<CapsuleCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /* ===========================================
+    Update() : FlipSprite(); isGrounded(); , 
+    FixedUpdate()
+    ===========================================*/
+    void Update() //logical operaions
     {
-        //animator.SetFloat("magnitude", rb.velocity.magnitude);
-       // animator.SetFloat("yVelocity", rb.velocity.y);
-       // rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y);
-        
+        currentSpeed = isCrouching ? crouchSpeed : moveSpeed; 
         FlipSprite();
 
         isGrounded();
-       
-        
     }
-    private void FixedUpdate() //faster than update? not sure
+    private void FixedUpdate() //for animation and movement 
     {    
-    // change the player movement/velocity after the inputs (horizontalMovement (updated in MOVE() ) * moveSpeed (static) )
-    rb.velocity = new Vector2(horizontalMovement * moveSpeed, rb.velocity.y); 
+    // change the player movement/velocity after the inputs (horizontalMovement (updated in MOVE() ) * current speend (updated in update() depending on weither crouch or not) )
+    rb.velocity = new Vector2(horizontalMovement * currentSpeed, rb.velocity.y); 
     //to enable the animations based on the new changed values of both : magnitude=X , yVelocity=Y
     animator.SetFloat("magnitude", Mathf.Abs(rb.velocity.x)); //makes magnitude always positive
     animator.SetFloat("yVelocity", rb.velocity.y);
+    animator.SetBool("IsCrouching", isCrouching);
     }
-
 
     void FlipSprite(){ //the name says it all...
         if ((isFacingRight && horizontalMovement < 0f) || (!isFacingRight && horizontalMovement > 0f)){            isFacingRight = !isFacingRight;
@@ -72,43 +89,52 @@ int jumpNumber= 1;
     //groundCheckSize is WHERE her feet are
     //groundLayer is important to check if the object's Layer is "Ground"
     //0f is an angle for something im not sure and prob not important
-    {
-        
+    { 
         //the object is ground, now u r able to jump again
         jumps = jumpNumber;
     }
    
     }
 
-
+    /* ==================================================
+    Functions to assign to the input system of the player
+    =====================================================*/
     public void Move(InputAction.CallbackContext context)
     { 
         horizontalMovement = context.ReadValue<Vector2>().x; 
         // reassign any horizantal Movement of the X velocity back in the FixedUpdate() method 
-        //im honestly not sure why not the Update method but i think the fixed one is kinda faster 
-        animator.ResetTrigger("jump");
+        //im honestly not sure why not the Update method but i think the fixed one is kinda faster   
     }
-    public void Jump(InputAction.CallbackContext context) //function to assign to the input system of the player
+    public void Jump(InputAction.CallbackContext context) 
     {
         if(jumps > 0) //if we are on the ground, ONLY, jump //doesn't allow multiple jumps
         {
-        if(context.performed){ //button is FULLY pressed
+        if(context.performed){ //button is FULLY pressed; interaction is complete
             rb.velocity = new Vector2(rb.velocity.x ,jumpPower); // change the player's Y velocity to the jumpPower
             jumps--;            
             animator.SetTrigger("jump"); // enable animation by checking the jumping condition
         }
-        if(context.canceled){ //button is NOT Fully pressed "Light tap"
+        if(context.canceled){ //button is NOT Fully pressed "Light tap"; interaction is not complete/interrupted
             rb.velocity = new Vector2(rb.velocity.x ,rb.velocity.y *0.5f); //divide the Y velocity by half,Not a full jump
             jumps--;  
-            animator.SetTrigger("jump");
         }
         }
     }
     public void Crouch(InputAction.CallbackContext context)
     { 
-        if(context.performed){          
-            animator.SetTrigger("Crouch"); 
-            
+        if (context.performed)  //button Pressed; Press is performed as an 'interaction'
+        {
+            isCrouching = true; // movement/current speed will be changed
+            animator.SetBool("IsCrouching", true);
+            bodySize.size = crouchingColliderSize;     // shrink the body's collider Size
+            headPosition.offset = crouchingColliderOffset; // move head's collider down
+        }
+        else if (context.canceled)  //button Released
+        {
+            isCrouching = false;
+            animator.SetBool("IsCrouching", false);
+            bodySize.size = standingColliderSize;     //reset the body's collider Size
+            headPosition.offset = standingColliderOffset; //reset the collider's Position
         }
 
     }
@@ -126,7 +152,5 @@ int jumpNumber= 1;
     Gizmos.color = Color.white;
     Gizmos.DrawCube(groundCheckPos.position,groundCheckSize); //groundCheck is an empty obj with a position 
 }
-
-
 
 }
