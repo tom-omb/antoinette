710a4b7aedaf672c638164f1e3f4307125dd10b8
Asma Ibrahim <asmaibra.cs@gmail.com>
2025-04-03 08:43:19 +0300

    Stove Fire, TMP, Level 4' Light
    - Fire will cause damage.
    - Text Mesh Pro imported + added a temporary font in the fonts file.
    - Added a temporary Level 4 scene with a light that would cause damage.

---
+++ Antoinette/Assets/Scripts/particleSystemDamage.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class particleStstemDamage : MonoBehaviour
+{
+
+    private float damageCooldown = 2f; // collision -> Damage -> one secound -> Damage again
+    private bool canTakeDamage = true;
+
+    void OnParticleCollision(GameObject other)  //the collision message should be enabled
+    {
+        if (other.CompareTag("Player") && canTakeDamage) 
+        {
+            AntHealth playerHealth = other.GetComponent<AntHealth>();
+ 
+            if (playerHealth != null)
+            {
+                playerHealth.HealthLost();
+                StartCoroutine(DamageCooldown());
+            }
+        }
+    }            
+    IEnumerator DamageCooldown() 
+    {
+        canTakeDamage = false;
+        yield return new WaitForSeconds(damageCooldown);
+        canTakeDamage = true;
+    } 
+}
